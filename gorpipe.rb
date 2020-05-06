8163535b68d75bcf1795fb03442c24e452ecc2b7
sigmar <sigmar@wuxinextcode.com>
2020-05-06 13:58:30 +0000

    e

--- gorpipe.rb
+++ gorpipe.rb
@@ -1,5 +1,6 @@
 class Gorpipe < Formula
   desc "Gorpipe"
+  name "Gorpipe"
   homepage "https://github.com/gorpipe/gor"
   url "https://sigmarkarl.github.io/gorpipe.tar.gz"
   sha256 "ba16b87de477e1aade8eb6a8dd92c1782d39e24e2c32bd456dc4a92b9da62084"
