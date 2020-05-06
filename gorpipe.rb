dc929f717d8b3dde2da9570cc3cb0d2b9c1d595f
sigmar <sigmar@wuxinextcode.com>
2020-05-06 14:10:35 +0000

    e

--- gorpipe.rb
+++ gorpipe.rb
@@ -4,8 +4,7 @@ class Gorpipe < Formula
   url "https://sigmarkarl.github.io/gorpipe.tar.gz"
   sha256 "ba16b87de477e1aade8eb6a8dd92c1782d39e24e2c32bd456dc4a92b9da62084"
   version "2.4.1"
-  name "SGorpipe"
-
+  
   def install
     system "mkdir", "-p", "/usr/local/Cellar/gorpipe"
     system "echo", "'bo'"
