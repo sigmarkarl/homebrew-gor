461fb78880605e0c14409383c91220f4ec675bab
sigmar <sigmar@wuxinextcode.com>
2020-05-07 11:28:25 +0000

    ngorpipe

--- gorpipe.rb
+++ gorpipe.rb
@@ -1,13 +1,12 @@
 class Gorpipe < Formula
-  desc "DGorpipe"
+  desc ""
   homepage "https://github.com/gorpipe/gor"
-  url "https://sigmarkarl.github.io/gorpipe.tar.gz"
-  sha256 "ba16b87de477e1aade8eb6a8dd92c1782d39e24e2c32bd456dc4a92b9da62084"
+  url "https://github.com/gorpipe/gor/releases/download/v2.4.1/gor-scripts-2.4.1.zip"
+  sha256 "ad3549bf9d1d75f55063e6899b29a7d75bf104f3f4df5c8bf2140c97b4d031f8"
   version "2.4.1"
 
   def install
-    system "mkdir", "-p", "/usr/local/Cellar/gorpipe/2.4.1"
-    system "cp", "gorpipe", "/usr/local/Cellar/gorpipe/2.4.1/gorpipe"
+    prefix.install Dir["*"]
     bin.install_symlink prefix/"gorpipe"
   end
 
---
+++ ngorpipe.rb
@@ -0,0 +1,17 @@
+class Ngorpipe < Formula
+  desc ""
+  homepage "https://github.com/gorpipe/gor"
+  url "https://sigmarkarl.github.io/gorpipe.tar.gz"
+  sha256 "ba16b87de477e1aade8eb6a8dd92c1782d39e24e2c32bd456dc4a92b9da62084"
+  version "2.4.1"
+
+  def install
+    system "mkdir", "-p", "/usr/local/Cellar/gorpipe/2.4.1"
+    system "cp", "gorpipe", "/usr/local/Cellar/gorpipe/2.4.1/gorpipe"
+    bin.install_symlink prefix/"gorpipe"
+  end
+
+  test do
+    system "gorpipe"
+  end
+end
