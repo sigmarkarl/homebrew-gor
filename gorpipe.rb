5ebb25153664e9a5b701497c931eae016289e82a
sigmar <sigmar@wuxinextcode.com>
2020-06-04 17:05:28 +0000

    v0.2.9

--- gorpipe.rb
+++ gorpipe.rb
@@ -1,9 +1,9 @@
 class Gorpipe < Formula
   desc ""
   homepage "https://github.com/gorpipe/gor"
-  url "https://github.com/sigmarkarl/sparkgor/releases/download/v0.2.4/gor-scripts-refs.tags.v0.2.4.zip"
-  sha256 "e83690515b13a08675a1fc15299bd6f04309d7783178a4f4c1ba40923802d4e4"
-  version "0.2.2"
+  url "https://github.com/sigmarkarl/sparkgor/releases/download/v0.2.9/gor-scripts-refs.tags.v0.2.9.zip"
+  sha256 "fdcfbf7c51f02f75345af1087bfb91165545d75a329bc3866161f3239a489116"
+  version "0.2.9"
 
   def install
     prefix.install Dir["*"]
