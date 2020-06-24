54b52694bc7c345b6e5a99d3f612ef063cac9ff6
sigmar <sigmar@wuxinextcode.com>
2020-06-24 17:21:57 +0000

    update to 0.5.2

--- gorpipe.rb
+++ gorpipe.rb
@@ -1,7 +1,7 @@
 class Gorpipe < Formula
   desc ""
   homepage "https://github.com/gorpipe/gor"
-  url "https://github.com/sigmarkarl/sparkgor/releases/download/v0.2.9/gor-scripts-refs.tags.v0.5.2.zip"
+  url "https://github.com/sigmarkarl/sparkgor/releases/download/v0.5.2/gor-scripts-refs.tags.v0.5.2.zip"
   sha256 "703e95cf9edbd3b8b59a93b3180a9da147178ea4a4774a911affb4ad794cd234 "
   version "0.5.2"
 
